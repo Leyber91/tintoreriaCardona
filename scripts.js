8619bb5e820212cc4a0da05ade144dccd97a334e
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-04-03 10:11:25 +0100

    Commit 0014. Tintoreria Cardona. Quick fix style III

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const languageToggle = document.createElement("span");
+    languageToggle.classList.add("language-toggle");
+    languageToggle.innerHTML = '<i class="fas fa-globe"></i>';
+    document.querySelector(".header-content").appendChild(languageToggle);
+
+    languageToggle.addEventListener("click", function () {
+        const languageSelector = document.querySelector(".language-selector");
+        languageSelector.classList.toggle("shown");
+    });
+});
